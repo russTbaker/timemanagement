5d0da5a4a2328d5d63d12471ff9c7c62c5733b53
Russ Baker <Russ.Baker@hds.com>
2016-02-26 17:19:19 -0700

    Changing name, or trying to.

--- server/src/main/java/com/rbc/timemanagmentservice/model/TimeSheet.java
+++ server/src/main/java/com/rbc/timemanagmentservice/model/TimeSheet.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 @Entity
 public class Timesheet {
-    public Timesheet() { }
+    public Timesheet() {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
